bc77b9795a93722f73136dc03694f62d0d2f1149
sdk40010 <sdk40010@gmail.com>
2019-03-25 23:12:39 +0900

    Rightが正常系となるEither を実装

--- src/main/scala/EitherStudy.scala
+++ src/main/scala/EitherStudy.scala
@@ -1,9 +1,14 @@
 object EitherStudy {
-
   sealed trait Either[+E, +A] {
-    def map[B](f: A => B): Either[E, B] = ???
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Left(a) => Left(a)
+      case Right(a) => Right(f(a))
+    }
 
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Left(a) => Left(a)
+      case Right(a) => f(a)
+    }
   }
 
   case class Left[+E](get: E) extends Either[E, Nothing]

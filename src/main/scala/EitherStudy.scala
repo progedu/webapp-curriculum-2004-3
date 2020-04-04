1d8bf66820622008b4c861202dd795074d857532
QWYNG <ikusawasi@gmail.com>
2020-04-04 12:22:27 +0900

    impl EitherStudy

--- src/main/scala/EitherStudy.scala
+++ src/main/scala/EitherStudy.scala
@@ -1,9 +1,19 @@
 object EitherStudy {
 
   sealed trait Either[+E, +A] {
-    def map[B](f: A => B): Either[E, B] = ???
+    def map[B](f: A => B): Either[E, B] = {
+      this match {
+        case Left(get)  => Left(get)
+        case Right(get) => Right(f(get))
+      }
+    }
 
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+      this match {
+        case Left(get)  => Left(get)
+        case Right(get) => f(get)
+      }
+    }
   }
 
   case class Left[+E](get: E) extends Either[E, Nothing]

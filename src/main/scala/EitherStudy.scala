f080af13439fbf47815717dd9cc39ee3e97f5550
Kazuki Nitta <nittakdub@gmail.com>
2022-04-06 16:38:37 +0900

    Eitherの実装

--- src/main/scala/EitherStudy.scala
+++ src/main/scala/EitherStudy.scala
@@ -1,9 +1,15 @@
 object EitherStudy {
 
   sealed trait Either[+E, +A] {
-    def map[B](f: A => B): Either[E, B] = ???
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Right(get) => Right(f(get))
+      case Left(get) => Left(get)
+    }
 
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Right(get) => f(get)
+      case Left(get) => Left(get)
+    }
   }
 
   case class Left[+E](get: E) extends Either[E, Nothing]

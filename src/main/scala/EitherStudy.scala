5d3f6b470a4aca829a03b209ee1cbc23dc2517cb
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 16:15:12 +0900

    map, flatMapの実装

--- src/main/scala/EitherStudy.scala
+++ src/main/scala/EitherStudy.scala
@@ -1,9 +1,15 @@
 object EitherStudy {
 
   sealed trait Either[+E, +A] {
-    def map[B](f: A => B): Either[E, B] = ???
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Right(x) => Right(f(x))
+      case Left(x) => Left(x)
+    }
 
-    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Right(x) => f(x)
+      case Left(x) => Left(x)
+    }
   }
 
   case class Left[+E](get: E) extends Either[E, Nothing]
